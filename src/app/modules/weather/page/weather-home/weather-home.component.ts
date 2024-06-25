b674b2ea3b52402d7b9e538d4d1ad16d3e793c0e
Yzak Carmo <yzak.carmo@gmail.com>
2024-06-24 21:35:44 -0300

    Criado WeathersData.interface

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {WeatherService} from "../../services/weather.service";
+import {WeatherData} from "../../../../models/interfaces/WeathersData.interface";
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit {
+  initialCity = 'São Paulo';
+  weatherData!: WeatherData;
 
+  constructor(private weatherService: WeatherService) {
+  }
+
+  ngOnInit(): void {
+    this.getWeatherData(this.initialCity);
+  }
+
+  getWeatherData(cityName: string): void {
+    this.weatherService.getWeatherData(cityName)
+      .subscribe({
+        next: (response)=> {
+          response && (this.weatherData = response);
+          console.log(this.weatherData);
+        },
+        error: (error) => console.log(error)
+      })
+  }
 }
